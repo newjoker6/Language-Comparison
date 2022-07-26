8c43bace09d570fde2773810b5c387f0b5efb047
Michael McGuire <40023411+newjoker6@users.noreply.github.com>
2022-07-26 06:36:54 -0700

    Removed unneeded var

--- CPP AGK Headers/template.cpp
+++ CPP AGK Headers/template.cpp
@@ -21,7 +21,6 @@ map<string, string> database;
 
 
 // Password Selection Pool
-//uString AlphaChoice$ = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,1,2,3,4,5,6,7,8,9,0,!,@,#,$,%,^,&,*,(,),-,=,_,+";
 const char alphanum[] = "0123456789!@#$%^&*abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 // Box IDs
